b6004da5c7000a885728f905d644a03f67b81040
Diogo <diogoarantescampos@hotmail.com>
2018-05-08 16:27:21 -0300

    Modified templates to display posts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd1efiv)mdghsl+e%)u_$an2d&sxfm-@hx)fx&x##p6v6)r+cc8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'DiogoArantes.pythonanywhere.com']
 
