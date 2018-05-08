30b7ebc99cf89f9795b2cd0f8b8889f93075afb8
Diogo <diogoarantescampos@hotmail.com>
2018-05-08 16:18:50 -0300

    Modified templates to display posts from database.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,4 +1,16 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
 import os
 
-from helloworld.wsgi import HelloWorldApplication
-application = HelloWorldApplication(application)
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+application = get_wsgi_application()
